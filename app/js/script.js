4fd9922212417e80619520104625b36fda4f1f25
KaidoSo <kaido.soomlais@gmail.com>
2021-03-09 16:20:10 +0200

    added slideshow.scss and initial positioning

--- app/js/script.js
+++ app/js/script.js
@@ -5,3 +5,14 @@ menu_btn.addEventListener('click', function(){
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 });
+
+$(document).ready(function(){
+    $('#slideshow .slick').slick({
+        autoplay: false,
+        speed: 1000,
+        prevArrow: '.arrow_prev',
+        nextArrow: '.arrow_next',
+        arrows: true,
+        dots: true,
+    });
+});
